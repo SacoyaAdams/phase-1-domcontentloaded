8900eb754133078eab4e648d9ad45b8c5210b4d2
+ <+>
2022-11-28 14:22:18 -0500

    commit tree 7ff25042ddc670e00c630d719ad92aee2fb2f376

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    document.querySelector('P').textContent = "This is really cool!"
+  });
+ 
+document.querySelector('P').textContent = "JavaScript is so cool. It lets me add text to my page programmatically."
